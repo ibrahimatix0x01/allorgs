d4a514cf294c6d48023d06a5d381164b4c222c15
ibrahimatix0x01 <96072328+ibrahimatix0x01@users.noreply.github.com>
2022-07-21 12:46:14 +0100

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,20 @@
+TARGET=$1
+if ! [ $TARGET ]; then
+  echo "[!] No target provided."
+  echo ">> $0 <targets.txt>"
+  exit 1
+fi
+OUT_DIR=$(pwd)
+mkdir $TARGET
+echo [*] Executing ALLORGS against: ${TARGET}
+echo "[*] Launching Subfinder"
+sub -dL $TARGET -o $OUT_DIR/$TARGET/subfinder.txt
+RES=$(cat subfinder.txt | wc -l)
+echo -e "\n[+] AllORGS complete with ${RES} results"
+echo "[+] Output are saved to: $OUT_DIR/$TARGET/subfinder.txt"
+echo [*] Executing ALLORGS against: All the domains
+cat $OUT_DIR/$TARGET/subfinder.txt | httprobe > $OUT_DIR/$TARGET/probed.txt
+echo "[+] Live subdomains are saved to: $OUT_DIR/$TARGET/probed.txt"
+nuclei -list $OUT_DIR/$TARGET/probed.txt -o $OUT_DIR/$TARGET/final.txt
+echo "[+] Final results are saved to: $OUT_DIR/$TARGET/final.txt"
+exit 0
